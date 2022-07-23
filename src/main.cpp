c95b4931a42c3cf02fa6871f3b9ea70f0e7c3253
Calvin Katt <katt.calvin@gmail.com>
2022-07-23 18:41:20 +0200

    Simple command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,17 @@
 #include "Printer.hpp"
 #include "CodeGenerator.hpp"
 #include "CodeGenerationDriver.hpp"
-int main() {
-    std::string file_name = "test.kal";
-    std::ifstream file_stream(file_name);
-    ParserDriver driver(file_name, file_stream);
+int main(int argc, char** argv) {
+    std::string file_name;
+    std::ifstream file_stream;
+    if (argc == 2) {
+        file_name = std::string(argv[1]);
+        file_stream = std::ifstream(file_name);
+    } else {
+        file_name = "input";
+    }
+
+    ParserDriver driver(file_name, argc == 2 ? file_stream : std::cin);
     int result = driver.parse();
     std::cout << "Parsing was " << (result == 0 ? "successful" : "unsuccessful") << "\n";
     if (result == 0) {
@@ -15,15 +22,17 @@ int main() {
         driver.getAST().accept(printer);
         try {
             CodeGenerationDriver codeDriver(driver.getAST());
-            codeDriver.optimize();
+            //codeDriver.optimize();
             std::cout << "Code Generated\n";
             codeDriver.getModule().print(llvm::errs(), nullptr);
-            codeDriver.compile("test.kal.o");
+            codeDriver.compile(file_name + ".o");
 
         } catch (...) {
             std::cerr << "There was an error compiling the code. Aborting\n";
             return 1;
         }
+    } else {
+        return 1;
     }
 
     
